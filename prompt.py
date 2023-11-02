1a182ffcbd0c44d07949b113195aeb39502fd204
tedirland <irlandth@gmail.com>
2023-11-02 12:19:40 -0500

    added a prompt.py file to abstract query

---
+++ prompt.py
@@ -0,0 +1,28 @@
+from langchain.vectorstores.chroma import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+embeddings = OpenAIEmbeddings()
+
+db = Chroma(
+    persist_directory="emb",
+    embedding_function=embeddings
+)
+
+retriever = db.as_retriever()
+
+chain = RetrievalQA.from_chain_type(
+    llm=chat,
+    retriever= retriever,
+    chain_type="stuff",
+)
+
+result = chain.run("What is an interesting fact about the English Language?")
+
+print(result)
+
